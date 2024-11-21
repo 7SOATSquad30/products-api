b5ec1a11abf5e93423cd479607f5dee794651090
Jonas Souza <jonasmzsouza@gmail.com>
2024-11-21 00:13:35 -0300

    feat: add list all categories

---
+++ src/main/java/br/com/fiap/grupo30/fastfood/products_api/domain/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.grupo30.fastfood.products_api.domain.repositories;
+
+import br.com.fiap.grupo30.fastfood.products_api.domain.entities.Category;
+import java.util.List;
+
+public interface CategoryRepository {
+    List<Category> findAll();
+}
--- src/test/java/br/com/fiap/grupo30/fastfood/products_api/ProductsApiApplicationTests.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/products_api/ProductsApiApplicationTests.java
@@ -1,7 +1,6 @@
 package br.com.fiap.grupo30.fastfood.products_api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
