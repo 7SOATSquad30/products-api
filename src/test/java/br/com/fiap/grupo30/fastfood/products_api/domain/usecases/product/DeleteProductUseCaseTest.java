879cc75f372f3fbfc38138c6f1fa7aa99e0fdc67
Jonas Souza <jonasmzsouza@gmail.com>
2024-11-25 13:51:16 -0300

    test: add shouldDeleteProductWhenIdIsValid scenario

---
+++ src/test/java/br/com/fiap/grupo30/fastfood/products_api/domain/usecases/product/DeleteProductUseCaseTest.java
@@ -0,0 +1,49 @@
+package br.com.fiap.grupo30.fastfood.products_api.domain.usecases.product;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import br.com.fiap.grupo30.fastfood.products_api.domain.entities.Product;
+import br.com.fiap.grupo30.fastfood.products_api.infrastructure.gateways.ProductGateway;
+import br.com.fiap.grupo30.fastfood.products_api.utils.ProductHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class DeleteProductUseCaseTest {
+
+    @Mock private ProductGateway productGateway;
+
+    @InjectMocks private DeleteProductUseCase deleteProductUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldDeleteProductWhenIdIsValid() {
+        // Arrange
+        Long validId = 1L;
+        Product product = ProductHelper.createProductDefaultWithId(1L);
+        when(productGateway.findById(validId)).thenReturn(product);
+
+        // Act
+        deleteProductUseCase.execute(productGateway, validId);
+
+        // Assert
+        verify(productGateway).delete(validId);
+    }
+
+    @Test
+    void shouldCallDeleteExactlyOnce() {
+        fail("Test not implemented");
+    }
+
+    @Test
+    void shouldNotCallOtherProductGatewayMethods() {
+        fail("Test not implemented");
+    }
+}
