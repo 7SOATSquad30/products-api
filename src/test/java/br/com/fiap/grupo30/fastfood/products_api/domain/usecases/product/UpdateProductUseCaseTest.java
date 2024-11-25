5f32d464f816c38586e3a270876f448341c8fd51
Jonas Souza <jonasmzsouza@gmail.com>
2024-11-25 11:32:00 -0300

    test: add shouldUpdateProductCategory scenario

--- src/test/java/br/com/fiap/grupo30/fastfood/products_api/domain/usecases/product/UpdateProductUseCaseTest.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/products_api/domain/usecases/product/UpdateProductUseCaseTest.java
@@ -104,7 +104,20 @@ class UpdateProductUseCaseTest {
 
     @Test
     void shouldUpdateProductCategory() {
-        fail("Test not implemented");
+        // Arrange
+        Category category = ProductHelper.createDefaultCategory();
+        Product product = ProductHelper.createProductWithId(1L);
+        ProductDTO productDTO = ProductHelper.createDefaultProductDTOWithId();
+
+        when(categoryGateway.findOne(CATEGORY_NAME)).thenReturn(category);
+        when(productGateway.findById(1L)).thenReturn(product);
+        when(productGateway.save(any(Product.class))).thenReturn(product);
+
+        // Act
+        updateProductUseCase.execute(productGateway, categoryGateway, productDTO);
+
+        // Assert
+        assertThat(product.getCategory()).isEqualTo(category);
     }
 
     @Test
