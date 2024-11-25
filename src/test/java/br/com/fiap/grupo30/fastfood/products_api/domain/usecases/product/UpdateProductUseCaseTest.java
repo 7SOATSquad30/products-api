945ab4b72c19e6ce2ab476c3e1fcd76ea1641c4a
Jonas Souza <jonasmzsouza@gmail.com>
2024-11-25 11:26:55 -0300

    test: add shouldUpdateProductImgUrl scenario

--- src/test/java/br/com/fiap/grupo30/fastfood/products_api/domain/usecases/product/UpdateProductUseCaseTest.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/products_api/domain/usecases/product/UpdateProductUseCaseTest.java
@@ -86,7 +86,20 @@ class UpdateProductUseCaseTest {
 
     @Test
     void shouldUpdateProductImgUrl() {
-        fail("Test not implemented");
+        // Arrange
+        Category category = ProductHelper.createDefaultCategory();
+        Product product = ProductHelper.createProductWithId(1L);
+        ProductDTO productDTO = ProductHelper.createDefaultProductDTOWithId();
+
+        when(categoryGateway.findOne(CATEGORY_NAME)).thenReturn(category);
+        when(productGateway.findById(1L)).thenReturn(product);
+        when(productGateway.save(any(Product.class))).thenReturn(product);
+
+        // Act
+        updateProductUseCase.execute(productGateway, categoryGateway, productDTO);
+
+        // Assert
+        assertThat(product.getImgUrl()).isEqualTo(productDTO.getImgUrl());
     }
 
     @Test
