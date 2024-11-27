3b1dee75e219c9d2cf72add5d608f5bde449ab49
Jonas Souza <jonasmzsouza@gmail.com>
2024-11-27 17:56:05 -0300

    test: add prePersistShouldSetCreatedAt scenario

---
+++ src/test/java/br/com/fiap/grupo30/fastfood/products_api/infrastructure/persistence/entities/ProductEntityTest.java
@@ -0,0 +1,20 @@
+package br.com.fiap.grupo30.fastfood.products_api.infrastructure.persistence.entities;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class ProductEntityTest {
+
+    @Test
+    void prePersistShouldSetCreatedAt() {
+        // Arrange
+        ProductEntity product = new ProductEntity();
+
+        // Act
+        product.prePersist();
+
+        // Assert
+        assertNotNull(product.getCreatedAt(), "CreatedAt should be set during prePersist");
+    }
+}
